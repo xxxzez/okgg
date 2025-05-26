033e4e35c5456ed4a39b5289d653c3151e1d15af
Matthew Bohush <matvei@wp-masters.com>
2025-05-26 07:48:05 +0300

    fix import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-'use client'
-
 import SidebarLeft from '@/components/SidebarLeft'
 import SidebarRight from '@/components/SidebarRight'
-import Header from '@/components/Header'
 import NewsGrid from '@/components/NewsGrid'
 import MainNews from '@/components/MainNews'
 
-import { useSession, signIn, signOut } from 'next-auth/react'
-
 export default function Home() {
-  const { data: session } = useSession()
-
   return (
     <>
       <div className="relative min-h-screen flex flex-col items-center bg-transparent">
