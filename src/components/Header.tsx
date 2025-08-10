37486052c1d1708f855a6cfe58b99c986ba7d06f
Matthew Bohush <matvei@wp-masters.com>
2025-08-10 21:40:28 +0300

    smth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,25 +2,17 @@
 
 import { useSession, signIn, signOut } from 'next-auth/react'
 import Link from 'next/link' 
+import Image from 'next/image'
 
 export default function Header() {
   const { data: session } = useSession()
 
   return (
-    <header className="sticky top-0 z-50 w-full backdrop-blur-md bg-black/70 border-b border-black/30 shadow-sm text-white font-semibold tracking-wide">
+    <header className="sticky top-0 z-50 w-full backdrop-blur-md bg-[#fa00ff]/70  shadow-sm text-white font-semibold tracking-wide">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-5">
         <Link href="/">
-          <div className="flex items-center gap-2">
-            <div className="flex items-center gap-2">
-              <div className="w-10 h-10 bg-gradient-to-r from-blue-500 to-green-500 rounded-full flex items-center justify-center">
-                <span className="text-white text-xl font-bold">ok</span>
-              </div>
-              <span className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
-                gg
-              </span>
-            </div>
-          </div>
-        </Link>
+           CyberHub
+      </Link>
         <nav className="flex gap-6">
           {['News', 'Events', 'Results', 'Ranking', 'Matchmaking'].map(
             (item) => (
